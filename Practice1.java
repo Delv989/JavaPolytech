f1141a7dafa37fc44f9a2aa450419f20f97cc650
Delv989 <153451933+Delv989@users.noreply.github.com>
2024-11-29 23:33:49 +0300

    task

---
+++ Practice1.java
@@ -0,0 +1,7 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        int a = 56;
+        int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+        System.out.println(i);
+    }
+}
