d1105f993570266ff765d2fab7111146c1439506
Delv989 <153451933+Delv989@users.noreply.github.com>
2024-12-04 21:02:37 +0300

    task

---
+++ A.java
@@ -0,0 +1,27 @@
+public class A {
+    public final int a = 42; // Инициализация при объявлении
+}
+
+//public class A {
+//    public final int a;
+//
+//    public A(int value) {
+//        this.a = value; // Инициализация в конструкторе
+//    }
+//}
+
+//public class A {
+//    public final int a;
+//
+//    {
+//        a = 10; // Инициализация в блоке инициализации
+//    }
+//}
+
+//public class A {
+//    public static final int a;
+//
+//    static {
+//        a = 100; // Инициализация в статическом блоке
+//    }
+//}
